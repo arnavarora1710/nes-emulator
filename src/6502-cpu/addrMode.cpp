505c9ecc04d7612b12356060d9f53e2634c70464
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2025-08-07 19:59:44 -0700

    6502: major bug fixes + added comments for weird indirect modes

--- src/6502-cpu/addrMode.cpp
+++ src/6502-cpu/addrMode.cpp
@@ -21,8 +21,9 @@ uint8_t ISA::IMM() {
 // Absolute Addressing
 // Access memory at the given address (need to store in uint16_t)
 uint8_t ISA::ABS() {
-    m_cpuState.absAddr = m_cpu.read(m_registers.PC++);
-    m_cpuState.absAddr = (m_cpuState.absAddr << 8) | m_cpu.read(m_registers.PC++);
+    const uint16_t loByte = m_cpu.read(m_registers.PC++);
+    const uint16_t hiByte = m_cpu.read(m_registers.PC++);
+    m_cpuState.absAddr = (hiByte << 8) | loByte;
     return 0;
 }
 
@@ -74,8 +75,7 @@ uint8_t ISA::ZP0() {
 // Read from the Zero Page (first 256 bytes) and add contents of X
 // Takes 1 less cycle as the address to be read is only 1 byte now
 uint8_t ISA::ZPX() {
-    m_cpuState.absAddr = m_cpu.read(m_registers.PC + m_registers.X);
-    m_registers.PC++;
+    m_cpuState.absAddr = m_cpu.read(m_registers.PC++) + m_registers.X;
     m_cpuState.absAddr &= 0x00FF;
     return 0;
 }
@@ -84,8 +84,7 @@ uint8_t ISA::ZPX() {
 // Read from the Zero Page (first 256 bytes) and add contents of Y
 // Takes 1 less cycle as the address to be read is only 1 byte now
 uint8_t ISA::ZPY() {
-    m_cpuState.absAddr = m_cpu.read(m_registers.PC + m_registers.Y);
-    m_registers.PC++;
+    m_cpuState.absAddr = m_cpu.read(m_registers.PC++) + m_registers.Y;
     m_cpuState.absAddr &= 0x00FF;
     return 0;
 }
@@ -111,32 +110,49 @@ uint8_t ISA::IND() {
 
     if (readAddressLo == 0x00FF) {
         // Simulate hardware bug
-        m_cpuState.absAddr = (m_cpu.read(readAddress & 0xFF00) << 8) | m_cpu.read(readAddress + 0);
+        const uint16_t actualAddressLo = m_cpu.read(readAddress);
+        const uint16_t actualAddressHi = m_cpu.read(readAddress & 0xFF00);
+        m_cpuState.absAddr = (actualAddressHi << 8) | actualAddressLo;
     } else {
-        m_cpuState.absAddr = (m_cpu.read(readAddress + 1) << 8) | m_cpu.read(readAddress + 0);
+        const uint16_t actualAddressLo = m_cpu.read(readAddress);
+        const uint16_t actualAddressHi = m_cpu.read(readAddress + 1);
+        m_cpuState.absAddr = (actualAddressHi << 8) | actualAddressLo;
     }
 
     return 0;
 }
 
+// Pre-Indexed Indirect X
+// Works a lot like Zero Page X
+// but needs to do an additional lookup
+// Also indexes a location in the zero page
 uint8_t ISA::IZX() {
     const uint16_t readAddress = m_cpu.read(m_registers.PC++);
 
     const uint16_t xReg = m_registers.X;
-    const uint16_t readAddressLo = m_cpu.read((readAddress + xReg) & 0x00FF);
-    const uint16_t readAddressHi = m_cpu.read((readAddress + xReg + static_cast<uint16_t>(1)) & 0x00FF);
+    const uint16_t addrLo = (readAddress + xReg) & 0x00FF;
+    const uint16_t addrHi = (readAddress + xReg + static_cast<uint16_t>(1)) & 0x00FF;
+    const uint16_t readAddressLo = m_cpu.read(addrLo);
+    const uint16_t readAddressHi = m_cpu.read(addrHi);
 
-    m_cpuState.absAddr = (readAddressLo << 8) | readAddressHi;
+    m_cpuState.absAddr = (readAddressHi << 8) | readAddressLo;
 
     return 0;
 }
 
+// Post-Indexed Indirect Y
+// Works similarly, but the offset is done
+// after the lookup
+// If a page boundary is crossed, an additional
+// cycle is required
 uint8_t ISA::IZY() {
     const uint16_t readAddress = m_cpu.read(m_registers.PC++);
-    const uint16_t readAddressLo = m_cpu.read(readAddress & 0x00FF);
-    const uint16_t readAddressHi = m_cpu.read((readAddress + 1) & 0x00FF);
+    const uint16_t addrLo = readAddress & 0x00FF;
+    const uint16_t addrHi = (readAddress + static_cast<uint16_t>(1)) & 0x00FF;
+    const uint16_t readAddressLo = m_cpu.read(addrLo);
+    const uint16_t readAddressHi = m_cpu.read(addrHi);
 
-    m_cpuState.absAddr = (readAddressLo << 8) | readAddressHi;
+    m_cpuState.absAddr = (readAddressHi << 8) | readAddressLo;
     m_cpuState.absAddr += m_registers.Y;
 
     if ((m_cpuState.absAddr & 0xFF00) != (readAddressHi << 8))
