58529180c00d61c52c7082a8e5397195e036cbb5
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2025-08-12 19:10:57 -0700

    6502: added more instructions

--- src/6502-cpu/opCodes.cpp
+++ src/6502-cpu/opCodes.cpp
@@ -35,13 +35,233 @@ uint8_t ISA::AND() {
     return 1;
 }
 
-uint8_t ISA::ASL() { return 0; } uint8_t ISA::BCC() { return 0; }
-uint8_t ISA::BCS() { return 0; } uint8_t ISA::BEQ() { return 0; } uint8_t ISA::BIT() { return 0; } uint8_t ISA::BMI() { return 0; }
-uint8_t ISA::BNE() { return 0; } uint8_t ISA::BPL() { return 0; } uint8_t ISA::BRK() { return 0; } uint8_t ISA::BVC() { return 0; }
-uint8_t ISA::BVS() { return 0; } uint8_t ISA::CLC() { return 0; } uint8_t ISA::CLD() { return 0; } uint8_t ISA::CLI() { return 0; }
-uint8_t ISA::CLV() { return 0; } uint8_t ISA::CMP() { return 0; } uint8_t ISA::CPX() { return 0; } uint8_t ISA::CPY() { return 0; }
-uint8_t ISA::DEC() { return 0; } uint8_t ISA::DEX() { return 0; } uint8_t ISA::DEY() { return 0; } uint8_t ISA::EOR() { return 0; }
-uint8_t ISA::INC() { return 0; }
+uint8_t ISA::ASL() {
+    m_cpu.fetch();
+    const auto result = static_cast<uint16_t>(m_cpuState.fetched) << 1;
+    m_cpu.setFlag(CPU::StatusBit::CARRY_BIT, (result & 0xFF00) > 0);
+    m_cpu.setFlag(CPU::StatusBit::ZERO_BIT, (result & 0x00FF) == 0x00);
+    m_cpu.setFlag(CPU::StatusBit::OVERFLOW_BIT, result & 0x80);
+
+    const auto instr = m_cpu.m_isa.getInstruction(m_cpuState.opcode);
+    if (instr.addrMode != &ISA::IMP) {
+        m_registers.A = result & 0x00FF;
+    } else {
+        m_cpu.write(m_cpuState.absAddr, result & 0x00FF);
+    }
+
+    return 0;
+}
+
+uint8_t ISA::BCC() {
+    if (m_cpu.getFlag(CPU::StatusBit::CARRY_BIT) == 0) {
+        m_cpuState.cycles++;
+        m_cpuState.absAddr = m_registers.PC + m_cpuState.relAddr;
+
+        const auto absAddrHi = m_cpuState.absAddr & 0xFF00;
+        const auto pcHi = m_registers.PC & 0xFF00;
+        if (absAddrHi != pcHi) m_cpuState.cycles++;
+
+        m_registers.PC = m_cpuState.absAddr;
+    }
+    return 0;
+}
+
+uint8_t ISA::BCS() {
+    if (m_cpu.getFlag(CPU::StatusBit::CARRY_BIT) == 1) {
+        m_cpuState.cycles++;
+        m_cpuState.absAddr = m_registers.PC + m_cpuState.relAddr;
+
+        const auto absAddrHi = m_cpuState.absAddr & 0xFF00;
+        const auto pcHi = m_registers.PC & 0xFF00;
+        if (absAddrHi != pcHi) m_cpuState.cycles++;
+
+        m_registers.PC = m_cpuState.absAddr;
+    }
+    return 0;
+}
+
+uint8_t ISA::BEQ() {
+    if (m_cpu.getFlag(CPU::StatusBit::ZERO_BIT) == 1) {
+        m_cpuState.cycles++;
+        m_cpuState.absAddr = m_registers.PC + m_cpuState.relAddr;
+
+        const auto absAddrHi = m_cpuState.absAddr & 0xFF00;
+        const auto pcHi = m_registers.PC & 0xFF00;
+        if (absAddrHi != pcHi) m_cpuState.cycles++;
+
+        m_registers.PC = m_cpuState.absAddr;
+    }
+    return 0;
+}
+
+uint8_t ISA::BIT() {
+    m_cpu.fetch();
+    const auto result = m_cpuState.fetched & m_registers.A;
+    m_cpu.setFlag(CPU::StatusBit::ZERO_BIT, (result & 0x00FF) == 0x00);
+    m_cpu.setFlag(CPU::StatusBit::SIGN_BIT, (m_cpuState.fetched << 7));
+    m_cpu.setFlag(CPU::StatusBit::OVERFLOW_BIT, (m_cpuState.fetched << 6));
+    return 0;
+}
+
+uint8_t ISA::BMI() {
+    if (m_cpu.getFlag(CPU::StatusBit::SIGN_BIT) == 1) {
+        m_cpuState.cycles++;
+        m_cpuState.absAddr = m_registers.PC + m_cpuState.relAddr;
+
+        const auto absAddrHi = m_cpuState.absAddr & 0xFF00;
+        const auto pcHi = m_registers.PC & 0xFF00;
+        if (absAddrHi != pcHi) m_cpuState.cycles++;
+
+        m_registers.PC = m_cpuState.absAddr;
+    }
+    return 0;
+}
+
+uint8_t ISA::BNE() {
+    if (m_cpu.getFlag(CPU::StatusBit::ZERO_BIT) == 0) {
+        m_cpuState.cycles++;
+        m_cpuState.absAddr = m_registers.PC + m_cpuState.relAddr;
+
+        const auto absAddrHi = m_cpuState.absAddr & 0xFF00;
+        const auto pcHi = m_registers.PC & 0xFF00;
+        if (absAddrHi != pcHi) m_cpuState.cycles++;
+
+        m_registers.PC = m_cpuState.absAddr;
+    }
+    return 0;
+}
+
+uint8_t ISA::BPL() {
+    if (m_cpu.getFlag(CPU::StatusBit::SIGN_BIT) == 0) {
+        m_cpuState.cycles++;
+        m_cpuState.absAddr = m_registers.PC + m_cpuState.relAddr;
+
+        const auto absAddrHi = m_cpuState.absAddr & 0xFF00;
+        const auto pcHi = m_registers.PC & 0xFF00;
+        if (absAddrHi != pcHi) m_cpuState.cycles++;
+
+        m_registers.PC = m_cpuState.absAddr;
+    }
+    return 0;
+}
+
+// TODO
+uint8_t ISA::BRK() { return 0; }
+
+uint8_t ISA::BVC() {
+    if (m_cpu.getFlag(CPU::StatusBit::OVERFLOW_BIT) == 0) {
+        m_cpuState.cycles++;
+        m_cpuState.absAddr = m_registers.PC + m_cpuState.relAddr;
+
+        const auto absAddrHi = m_cpuState.absAddr & 0xFF00;
+        const auto pcHi = m_registers.PC & 0xFF00;
+        if (absAddrHi != pcHi) m_cpuState.cycles++;
+
+        m_registers.PC = m_cpuState.absAddr;
+    }
+    return 0;
+}
+
+uint8_t ISA::BVS() {
+    if (m_cpu.getFlag(CPU::StatusBit::OVERFLOW_BIT) == 1) {
+        m_cpuState.cycles++;
+        m_cpuState.absAddr = m_registers.PC + m_cpuState.relAddr;
+
+        const auto absAddrHi = m_cpuState.absAddr & 0xFF00;
+        const auto pcHi = m_registers.PC & 0xFF00;
+        if (absAddrHi != pcHi) m_cpuState.cycles++;
+
+        m_registers.PC = m_cpuState.absAddr;
+    }
+    return 0;
+}
+
+uint8_t ISA::CLC() {
+    m_cpu.setFlag(CPU::StatusBit::CARRY_BIT, false);
+    return 0;
+}
+
+uint8_t ISA::CLD() {
+    m_cpu.setFlag(CPU::StatusBit::DECIMAL_BIT, false);
+    return 0;
+}
+
+uint8_t ISA::CLI() {
+    m_cpu.setFlag(CPU::StatusBit::INTERRUPT_DISABLE_BIT, false);
+    return 0;
+}
+
+uint8_t ISA::CLV() {
+    m_cpu.setFlag(CPU::StatusBit::OVERFLOW_BIT, false);
+    return 0;
+}
+
+uint8_t ISA::CMP() {
+    m_cpu.fetch();
+    const auto result = static_cast<uint16_t>(m_registers.A - m_cpuState.fetched);
+    m_cpu.setFlag(CPU::StatusBit::CARRY_BIT, m_registers.A >= m_cpuState.fetched);
+    m_cpu.setFlag(CPU::StatusBit::ZERO_BIT, (result & 0x00FF) == 0);
+    m_cpu.setFlag(CPU::StatusBit::OVERFLOW_BIT, result & 0x0080);
+    return 1;
+}
+
+uint8_t ISA::CPX() {
+    m_cpu.fetch();
+    const auto result = static_cast<uint16_t>(m_registers.X - m_cpuState.fetched);
+    m_cpu.setFlag(CPU::StatusBit::CARRY_BIT, m_registers.X >= m_cpuState.fetched);
+    m_cpu.setFlag(CPU::StatusBit::ZERO_BIT, (result & 0x00FF) == 0);
+    m_cpu.setFlag(CPU::StatusBit::OVERFLOW_BIT, result & 0x0080);
+    return 1;
+}
+
+uint8_t ISA::CPY() {
+    m_cpu.fetch();
+    const auto result = static_cast<uint16_t>(m_registers.Y - m_cpuState.fetched);
+    m_cpu.setFlag(CPU::StatusBit::CARRY_BIT, m_registers.Y >= m_cpuState.fetched);
+    m_cpu.setFlag(CPU::StatusBit::ZERO_BIT, (result & 0x00FF) == 0);
+    m_cpu.setFlag(CPU::StatusBit::OVERFLOW_BIT, result & 0x0080);
+    return 1;
+}
+
+uint8_t ISA::DEC() {
+    m_cpu.fetch();
+    const auto result = m_cpuState.fetched - 1;
+    m_cpu.write(m_cpuState.absAddr, result & 0x00FF);
+    m_cpu.setFlag(CPU::StatusBit::OVERFLOW_BIT, (result & 0x00FF) == 0x0000);
+    m_cpu.setFlag(CPU::StatusBit::SIGN_BIT, result & 0x0080);
+    return 0;
+}
+
+uint8_t ISA::DEX() {
+    m_registers.X--;
+    m_cpu.setFlag(CPU::StatusBit::ZERO_BIT, m_registers.X == 0);
+    m_cpu.setFlag(CPU::StatusBit::SIGN_BIT, m_registers.X & 0x80);
+    return 0;
+}
+
+uint8_t ISA::DEY() {
+    m_registers.Y--;
+    m_cpu.setFlag(CPU::StatusBit::ZERO_BIT, m_registers.Y == 0);
+    m_cpu.setFlag(CPU::StatusBit::SIGN_BIT, m_registers.Y & 0x80);
+    return 0;
+}
+
+uint8_t ISA::EOR() {
+    m_cpu.fetch();
+    m_registers.A = m_registers.A ^ m_cpuState.fetched;
+    m_cpu.setFlag(CPU::StatusBit::ZERO_BIT, m_registers.A == 0);
+    m_cpu.setFlag(CPU::StatusBit::SIGN_BIT, m_registers.A & 0x80);
+    return 1;
+}
+
+uint8_t ISA::INC() {
+    m_cpu.fetch();
+    const auto result = m_cpuState.fetched + 1;
+    m_cpu.write(m_cpuState.absAddr, result & 0x00FF);
+    m_cpu.setFlag(CPU::StatusBit::OVERFLOW_BIT, (result & 0x00FF) == 0x0000);
+    m_cpu.setFlag(CPU::StatusBit::SIGN_BIT, result & 0x0080);
+    return 0;
+}
 
 uint8_t ISA::INX() {
     m_registers.X++;
